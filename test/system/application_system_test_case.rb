b5f9c9523fe89fba2c8327b13a8c23011e5dc228
mrkun10fu0-3030 <yyamahara13@gmail.com>
2026-06-16 09:52:43 +0900

    application_system_test_caseを正しく追加

--- test/system/application_system_test_case.rb
+++ test/system/application_system_test_case.rb
@@ -1,7 +1,5 @@
-cat > test/system/application_system_test_case.rb << 'EOF'
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :rack_test
 end
-EOF
